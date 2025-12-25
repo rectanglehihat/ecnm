80711959365ae933587a47d5d6e8fc36631141eb
rectanglehihat <rectanglehihat@gmail.com>
2025-12-25 23:41:48 +0900

    chore: hundred-key-statistics 페이지 주석

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,12 @@ export default function Home() {
 					<p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">모두의 국가 경제 통계</p>
 				</div>
 				<div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-					<Button
+					{/* <Button
 						href="/hundred-key-statistics"
 						className="flex h-12 w-fit items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
 					>
 						100대 통계지표
-					</Button>
+					</Button> */}
 					<Link
 						href="/cpi"
 						className="flex h-12 w-fit items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
