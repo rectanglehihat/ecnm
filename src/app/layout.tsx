e5bc667e289424cb9f11175f2f868227204f49ff
rectanglehihat <rectanglehihat@gmail.com>
2026-01-18 23:20:20 +0900

    feat: Header 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Header from '@/components/Header';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -27,6 +28,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-zinc-50 dark:bg-black text-black dark:text-zinc-50 mx-auto flex max-w-6xl flex-col gap-6 px-6 py-10`}
 			>
+				<Header />
 				{children}
 			</body>
 		</html>
