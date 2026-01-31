95ee8e0f579ae3e065506fcadc74bc8c64eb1b48
rectanglehihat <rectanglehihat@gmail.com>
2026-01-31 23:38:09 +0900

    style: h1 css 수정

--- src/app/cpi/page.tsx
+++ src/app/cpi/page.tsx
@@ -18,7 +18,7 @@ export default async function CpiPage() {
 
 	return (
 		<main className="space-y-8">
-			<h1 className="text-xl font-bold sm:text-2xl">소비자물가지수(CPI)</h1>
+			<h1 className="text-xl font-bold sm:text-2xl m-0">소비자물가지수(CPI)</h1>
 			<section className="py-6 sm:py-10">
 				<div className="flex flex-col gap-2 rounded-lg bg-gray-200 p-4 text-sm text-gray-600 sm:p-5 sm:text-base">
 					<p className="font-semibold">🍎 2020=100</p>
