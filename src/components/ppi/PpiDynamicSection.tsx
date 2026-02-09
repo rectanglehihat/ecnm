51ab2e069a6d504c6a83e3fb85bd3d09d0299d2a
rectanglehihat <rectanglehihat@gmail.com>
2026-02-10 02:20:33 +0900

    chore: console.log 삭제

--- src/components/ppi/PpiDynamicSection.tsx
+++ src/components/ppi/PpiDynamicSection.tsx
@@ -19,8 +19,6 @@ const DynamicSection = async ({ itemCodes, name, parentCode }: DynamicSectionPro
 		stats = {};
 	}
 
-	console.log('2222 stats', stats);
-
 	if (Object.keys(stats).length === 0) {
 		return (
 			<section className="rounded-xl border border-zinc-200 bg-white p-4 shadow-sm dark:border-zinc-800 dark:bg-zinc-900">
