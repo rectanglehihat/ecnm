12572b117d133090f9b1bdd0b1abadf49aab290c
rectanglehihat <rectanglehihat@gmail.com>
2026-02-26 13:37:29 +0900

    fix: ppi페이지 및 PpiTopLevelButtons 복원

--- src/components/ppi/PpiTopLevelButtons.tsx
+++ src/components/ppi/PpiTopLevelButtons.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import Button from '@/components/ui/Button';
-import { PpiStatisticTableListType } from '@/lib/ppi/fetchPpiStatisticTableList';
+import { PpiItemHierarchy } from '@/lib/ppi/fetchPpiItemCodes';
 
 type Props = {
-	data: PpiStatisticTableListType;
+	data: PpiItemHierarchy[];
 };
 
 const PpiTopLevelButtons = ({ data }: Props) => {
@@ -12,7 +12,7 @@ const PpiTopLevelButtons = ({ data }: Props) => {
 
 	return (
 		<div className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
-			{/* {data.map((node) => (
+			{data.map((node) => (
 				<Button
 					key={node.code}
 					href={`/ppi/${node.code}?code=${Object.values(node.children ?? {})[0]?.ITEM_CODE ?? ''}&name=${node.name}`}
@@ -20,13 +20,7 @@ const PpiTopLevelButtons = ({ data }: Props) => {
 				>
 					{node.name}
 				</Button>
-			))} */}
-			<Button
-				href={`/ppi/${data.STAT_CODE}?code=${data.STAT_CODE ?? ''}&name=${data.STAT_NAME}`}
-				className="flex h-auto w-full items-center justify-center rounded-full bg-foreground px-4 py-2.5 text-sm text-background hover:bg-[#383838] dark:hover:bg-[#ccc] sm:px-6 sm:text-base"
-			>
-				{data.STAT_NAME}
-			</Button>
+			))}
 		</div>
 	);
 };
